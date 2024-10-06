8fdd2c55611eda517c107ff28095a48fda3f45a5
mh2011-maisie <mh2011.music@gmail.com>
2024-10-06 08:34:18 +0100

    Completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+const bulbs = document.querySelectorAll(".lightbulb");
+let counter = document.querySelector("h3");
+let count = 0;
+
+for (bulb of bulbs){
+    bulb.addEventListener("click", function(){
+        this.classList.toggle("active")
+        count ++
+        if (count == 1){            
+            counter.innerHTML =  `You've clicked the light ${count} time`
+        }else{
+            counter.innerHTML =  `You've clicked the light ${count} times`
+        }
+    })
+}
+
+
